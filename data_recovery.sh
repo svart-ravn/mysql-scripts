b37938b89ebfd7ff3e2c8bf075775bc5fb39d48c
svart-ravn <srv.db.dev@gmail.com>
2016-07-03 09:55:19 -0700

    straightforward implementation done

--- data_recovery.sh
+++ data_recovery.sh
@@ -4,6 +4,7 @@
 PK=id
 DB=$1
 TABLE=$2
+DEST_TABLE="recovered_${TABLE}"
 
 
 MIN_ID=
@@ -11,11 +12,21 @@ MAX_ID=
 
 MYSQL_CMD="mysql $DB -BN"
 
+
 # --------------------------------------------------------------------------------------------------------------
 function check_options(){
+   if [ -z "$DB" ]; then
+      error "Use \$1 to pass DB name"
+      return 1
+   fi
+   
+   if [ -z "$TABLE" ]; then
+      error "Use \$2 to pass table name"
+      return 1
+   fi
+
    return 0
 }
-
 
 
 # --------------------------------------------------------------------------------------------------------------
@@ -32,8 +43,8 @@ function info(){
 # --------------------------------------------------------------------------------------------------------------
 function wait_for_mysql(){
    local RC=1
-   while [ RC -eq 1 ]; do
-      $MYSQL_CMD -e "select 1" >/dev/null
+   while [ $RC -eq 1 ]; do
+      $MYSQL_CMD -e "select 1" >/dev/null 2>&1
       RC=$?
       error "Waiting for MySQL to be started...."
       sleep 1
@@ -55,22 +66,38 @@ function init(){
    MAX_ID=$(echo $VAL | awk '{print $2}')
    info "min_id=$MIN_ID, max_id=$MAX_ID"
 
+
+   $MYSQL_CMD -e "drop table if exists $DEST_TABLE"
+   $MYSQL_CMD -e "create table if not exists $DEST_TABLE like $TABLE"
+
    return 0
 }
 
 
 function recover(){
+   for ID in $(seq $MIN_ID $MAX_ID); do 
+
+      $MYSQL_CMD -e "insert into $DEST_TABLE select * from $TABLE where $PK=$ID" >/dev/null 2>&1
+      if [ $? -eq 0 ]; then
+         info "$ID is ok"
+      else
+         error "$ID is not migrated"
+         wait_for_mysql
+      fi
+   done
+
    return 0
 }
 
-# ---------------------------------------------- MAIN ----------------------------------------------------------
 
+# ---------------------------------------------- MAIN ----------------------------------------------------------
 check_options || exit 1
 
 
-init
+init || exit 1
 
 
+info "Starting recovery..."
 recover
 
 
